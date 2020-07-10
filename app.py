625fa124e7a4c5421a54c126458b513819a47edd
Ahmed-10 <ahmedalaa.10@hotmail.com>
2020-07-10 19:02:39 +0200

    create an interactive web application to add and display tasks in To-Do list

---
+++ app.py
@@ -0,0 +1,56 @@
+# from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, jsonify, abort
+from flask_sqlalchemy import SQLAlchemy
+import sys
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/todoapp'
+
+# db = SQLAlchemy(app, session_options={"expire_on_commit": False})
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}>'
+
+
+db.create_all()
+
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    # description = request.form.get('description', '')
+    # todo = Todo(description=description)
+    # db.session.add(todo)
+    # db.session.commit()
+    # return redirect(url_for('index'))
+    error = False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+
+    if error:
+        abort(400)
+    else:
+        return jsonify(body)
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', data=Todo.query.all())
