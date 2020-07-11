7ecd89372fbb9412cc640f11bb47483f1ee53fb8
Ahmed-10 <ahmedalaa.10@hotmail.com>
2020-07-11 14:48:37 +0200

    add migrating to the project using flask_migrate

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask import Flask, render_template, request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)
@@ -10,16 +11,21 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:54
 db = SQLAlchemy(app)
 
 
+migrate = Migrate(app, db)
+db.create_all()
+
+
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
 
-db.create_all()
+# db.create_all()
 
 
 @app.route('/todos/create', methods=['POST'])
